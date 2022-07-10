5625abf630630b595c6468e4a4d29f9db43b7494
Junior Schmidt <junior.schmidt.99@gmail.com>
2022-07-10 19:48:29 -0300

    Create database.sql

---
+++ database/database.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS api;
+use api;
+
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE user (
+    id INT auto_increment primary key unique,
+    name varchar(200) not null,
+    username varchar(200) not null unique,
+    email varchar(200) not null unique,
+    password varchar(200) not null unique,
+    createdAt timestamp default current_timestamp()
+) ENGINE=INNODB;
